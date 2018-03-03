e8abff7968c52174be7fab1b536422a560ff2738
Travis Maupin <maupintcm@gmail.com>
2018-03-03 04:05:31 -0600

    Toolbar finally works

--- app/src/main/java/com/example/user/project_tweekometer/MainActivity.java
+++ app/src/main/java/com/example/user/project_tweekometer/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.user.project_tweekometer;
 
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
