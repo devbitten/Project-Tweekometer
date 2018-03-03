68e87d5e8a6bd5b7e2a5685362fd22f36a77250c
Dillion Driskell <dill.driskell@gmail.com>
2018-03-03 05:54:09 -0600

    Moving json to another machine to test

--- app/src/main/java/com/example/user/project_tweekometer/MainActivity.java
+++ app/src/main/java/com/example/user/project_tweekometer/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.user.project_tweekometer;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO : DELETE THIS, images should be 300x300
+    // TODO : DO SOMETHING WITH JSON PARSER
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadProducts();
+    }
+
+    /**
+     * This method loads the 'Product' content
+     */
+    public void loadProducts(/*View view*/){
+
+        Resources res = getResources();
+
+        InputStream is = res.openRawResource(R.raw.products);
+
+        Scanner scanner = new Scanner(is);
+
+        StringBuilder builder = new StringBuilder();
+
+        while(scanner.hasNextLine()) {
+            builder.append(scanner.nextLine());
+        }
+
+        String j = builder.toString();
+        try {
+            JSONObject root = new JSONObject(j);
+            System.out.println(root.getJSONObject("Products");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
